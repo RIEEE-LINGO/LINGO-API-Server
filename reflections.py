27b41e3cf6ef792de6c7b46f6f3371e192c23ab1
Mark Hills <hillsma@appstate.edu>
2024-02-13 11:30:50 -0500

    Add delete and filtering support for Reflections, resolves #3

--- reflections.py
+++ reflections.py
@@ -3,9 +3,16 @@ from config import db
 from lingo.models import Reflection, reflection_schema, reflections_schema
 
 
-def get_all(word_id):
-    reflections_for_word = Reflection.query.where(Reflection.word_id == word_id).all()
-    return reflections_schema.dump(reflections_for_word)
+def get_all(word_id, filter = "onlyActive"):
+    if filter.lower() == "onlyactive":
+        reflections_for_word = Reflection.query.where(Reflection.word_id == word_id, Reflection.active == True).all()
+        return reflections_schema.dump(reflections_for_word)
+    elif filter.lower() == "onlydeleted":
+        reflections_for_word = Reflection.query.where(Reflection.word_id == word_id, Reflection.active == False).all()
+        return reflections_schema.dump(reflections_for_word)
+    else:
+        reflections_for_word = Reflection.query.where(Reflection.word_id == word_id).all()
+        return reflections_schema.dump(reflections_for_word)
 
 
 def get(word_id, reflection_id):
@@ -15,7 +22,8 @@ def get(word_id, reflection_id):
         return reflection_schema.dump(reflection)
     else:
         abort(
-            404, f"Reflection with id {reflection_id} for word with id {word_id} not found"
+            404,
+            f"Reflection with id {reflection_id} for word with id {word_id} not found"
         )
 
 
@@ -31,22 +39,25 @@ def update(word_id, reflection_id, reflection):
     existing_reflection = Reflection.query.where(Reflection.id == reflection_id, Reflection.word_id == word_id).one_or_none()
     if existing_reflection:
         update_reflection = reflection_schema.load(reflection, session=db.session)
-        existing_reflection.reflection = update_reflection.reflection
         db.session.merge(existing_reflection)
         db.session.commit()
         return reflection_schema.dump(existing_reflection), 201
     else:
         abort(
-            404, f"Reflection with id {reflection_id} for word with id {word_id} not found"
+            404,
+            f"Reflection with id {reflection_id} for word with id {word_id} not found"
         )
 
 
 def delete(word_id, reflection_id):
     existing_reflection = Reflection.query.where(Reflection.id == reflection_id, Reflection.word_id == word_id).one_or_none()
     if existing_reflection:
-        reflections_schema.delete(existing_reflection)
+        existing_reflection.active = False
+        db.session.merge(existing_reflection)
+        db.session.commit()
         return True
     else:
         abort(
-            404, f"Reflection with id {reflection_id} for word with id {word_id} not found"
+            404,
+            f"Reflection with id {reflection_id} for word with id {word_id} not found"
         )
