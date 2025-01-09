20baf5bd751edf0f92dd1a8ae57005d21aa1e54a
Mark Hills <hillsma@appstate.edu>
2025-01-09 14:49:14 -0500

    Fix bug with missing param when creating teams

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         with app.app.app_context():
             db.create_all()
             users = create_users(db)
-            teams = create_teams(db)
+            teams = create_teams(db, users)
             words = create_words(db, teams[0].id)
 
     app.run(host="0.0.0.0", port=8000, debug=True)
